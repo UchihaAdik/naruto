7200af0a3a3a8ee4f8ac70b50e97d8f9af8989fc
UchihaAdik <adilvar2001@gmail.com>
2023-11-29 18:53:21 +0600

    gg

--- лучший первый проект .js
+++ лучший первый проект .js
@@ -1,33 +1,33 @@
 
-const shinobiWorld = [
+let shinobiWorld = [
     {
         name:'Boruto',clan:'Uzumaki',gender:'Male',rang: 'Genin',vallage: 'Leafy Village',age: 16,
         img:"https://w0.peakpx.com/wallpaper/612/169/HD-wallpaper-anime-naruto-2023-ai.jpg"
         ,
-        color: "#f79e5c"
+        color: "#f79e5c",about:'Клан Узумаки (яп. うずまき一族, Удзумаки Ичизоку) — самый известный клан Деревни Скрытого Водоворота. С момента его расформирования большинство его известных членов проживает в Конохагакуре, а Пятая и Седьмой Хокаге являются потомками клана.'
     },
     {
          name:'Saske', clan:'Uchiha', gender:'Male', rang: 'Genin', vallage: 'Leafy Village', age: 17,
          img:"https://i.pinimg.com/originals/59/8e/27/598e27f6965ec0732162d90ebbe3b473.jpg"
         ,
-        color: "#e610e0"
+        color: "#e610e0",about:"Клан Учиха (яп. うちは一族, Учиха Ичизоку) — один из четырёх благородных кланов Конохагакуре,[1] который прославился как сильнейший клан в деревне благодаря своему Шарингану и природному боевому мастерству. Лишь единицы Учих дожили до нынешнего дня."
     },
     {
         name:'Sakura', clan:'Haruno', gender:'Female', rang: 'Jonin', vallage: 'Leafy Village', age: 20,
         img:"https://4kwallpapers.com/images/wallpapers/sakura-haruno-naruto-5k-3840x2160-12184.jpg"
         ,
-        color: "#a70c44"
+        color: "#a70c44",about:"Харуно, будучи потомками Асуры Ооцуцуки, также имеет отдаленные родственные связи с кланами Сенджу и Узумаки. В течение многих лет Узумаки, Сенджу и Харуно поддерживали тесные связи, а члены кланов время от времени женились между собой."
     },
     {
         name:'Kakashi', clan:'hatake', gender:'Male', rang: 'Sennin', vallage: 'Leafy Village', age: 25,
         img:"https://c4.wallpaperflare.com/wallpaper/354/956/929/anime-naruto-kakashi-hatake-wallpaper-preview.jpg"
         ,
-        color: "#313b47"
+        color: "#313b47",about:" Клан Хатаке (яп. はたけ一族, Хатаке Ичизоку). Его единственные известные члены — прославленные ниндзя: Белый Клык Конохи Сакумо и его сын Копирующий Ниндзя Какаши. Последний позже станет Шестым Хокаге после окончания Четвёртой Мировой Войны Шиноби. "
     },
     {
         name:'Madara', clan:'Uchiha', gender:'Male', rang: 'dancer(died)',vallage: 'Leafy Village',
         img: "https://c4.wallpaperflare.com/wallpaper/501/399/582/anime-naruto-madara-uchiha-wallpaper-preview.jpg", 
-        color: "#612b71"
+        color: "#612b71",about:"Клан Учиха (яп. うちは一族, Учиха Ичизоку) — один из четырёх благородных кланов Конохагакуре,[1] который прославился как сильнейший клан в деревне благодаря своему Шарингану и природному боевому мастерству. Лишь единицы Учих дожили до нынешнего дня."
     },
     {
         name:'Itachi',
@@ -36,16 +36,61 @@ const shinobiWorld = [
         rang: 'Killer',
         vallage: 'Leafy Village',
         img: "https://c4.wallpaperflare.com/wallpaper/916/812/1002/naruto-uchiha-itachi-mangekyou-sharingan-wallpaper-preview.jpg" ,
-        color: "#1f092d"
+        color: "#1f092d",about:"Клан Учиха (яп. うちは一族, Учиха Ичизоку) — один из четырёх благородных кланов Конохагакуре,[1] который прославился как сильнейший клан в деревне благодаря своему Шарингану и природному боевому мастерству. Лишь единицы Учих дожили до нынешнего дня."
     },
     {
-        name:'Sasori',
-        clan:'',
+        name:'akatsuki',
+        clan:'akatsuki',
         gender:'Male',
         rang: 'Kuklovod(died)',
         vallage: 'Sand Village',
         img: "https://wallpapers.com/images/hd/akatsuki-splattered-with-red-and-black-0tydaaioqxabopb0.jpg" ,
-        color: "#ae030c"
+        color: "#ae030c",about:"Акацуки — легендарная группировка элитных ниндзя. В организации собрались самые мистические и загадочные персонажи-антагонисты, которые полюбились фанатам манги. Масаси Кисимото создал уникальное творение, в котором выделяется группировка Акацуки."
+        
+    },
+    {
+        name:'Darui',
+        clan:'',
+        gender:'Male',
+        rang: 'jonin',
+        vallage: 'Rain Village',
+        image: "https://static.wikia.nocookie.net/naruto/images/4/45/Darui.png/revision/latest?cb=20150624100502&path-prefix=ru" 
+        
+    },
+    {
+        name:'Karin',
+        clan:'Uzumaki',
+        gender:'Female',
+        rang: 'Genin',
+        vallage: 'Orochimaru lab',
+        image: "https://static.wikia.nocookie.net/naruto/images/6/6e/Karin_p3.png/revision/latest?cb=20190410180505&path-prefix=ru" 
+        
+    },
+    {
+        name:'Sai',
+        clan:'',
+        gender:'Male',
+        rang: 'Jonin',
+        vallage: 'Leafy Village',
+        image: "https://static.wikia.nocookie.net/naruto/images/6/6c/Sai_Part_III.png/revision/latest?cb=20201206114228&path-prefix=ru" 
+        
+    },
+    {
+        name:'Momshiki',
+        clan:'Otsusiki',
+        gender:'Male',
+        rang: 'God(died)',
+        vallage: 'Space',
+        image: "https://static.wikia.nocookie.net/naruto/images/1/13/Arco_Versus_Momoshiki.png/revision/latest?cb=20171210180323&path-prefix=ru" 
+        
+    },
+    {
+        name:'Toneri',
+        clan:'Otsusuki',
+        gender:'Male',
+        rang: '',
+        vallage: 'Moon',
+        image: "https://static.wikia.nocookie.net/anime-characters-fight/images/0/0f/Ootsutsuki_Toneri_%282%29.png/revision/latest?cb=20190404144842&path-prefix=ru" 
         
     },
 
@@ -56,25 +101,102 @@ const shinobiWorld = [
 
 
 
-const $carshinobiBottom = document.querySelector('.carshinobidgrup')
 
-function ganareitcards(arr){
-    const res = arr.map(item =>`
-    <div class="card" style="background-color:${item.color}">
-        <div class="saction">
-        <img class="card_img" src=${item.img} />
-        <div class="sactionText">
-            <h3> name: ${item.name}</h3>
-            <h3> clan: ${item.clan}</h3>
-            <h3> gender: ${item.gender}</h3>
-            <h3> rang: ${item.rang}</h3>
-            <h3> vallage: ${item.vallage}</h3>
-            <h3> age: ${item.age}</h3>
-        </div>
-        </div>
-    </div>
-    `
-    ).join('')
-    return res
-}
-$carshinobiBottom.insertAdjacentHTML('beforebegin', ganareitcards(shinobiWorld))
+
+
+
+
+const $carshinobiBottom = document.querySelector('.carshinobidgrup');
+const modal = document.querySelector('.modal');
+
+  
+  function generateCardHTML(arr) {
+    return arr.map(item => `
+      <div class="card" style="background-color:${item.color}">
+          <div class="saction">
+              <img class="card_img" src=${item.img} />
+              <div class="sactionText">
+                  <h3> name: ${item.name}</h3>
+                  <h3> clan: ${item.clan}</h3>
+                  <h3> gender: ${item.gender}</h3>
+                  <h3> rang: ${item.rang}</h3>
+                  <h3> vallage: ${item.vallage}</h3>
+                  <h3> age: ${item.age}</h3>
+                  <button class="btnshow" data-name="${item.name}">Узнать больше</button>
+              </div>
+          </div>
+      </div>
+    `).join('');
+  }
+  
+  function initEventListeners() {
+    const btnShowList = document.querySelectorAll('.btnshow');
+  
+    btnShowList.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        const characterName = btn.dataset.name;
+        const character = shinobiWorld.find(item => item.name === characterName);
+  
+        fillModal(character);
+  
+        modal.classList.add('clickedModal');
+      });
+    });
+  
+    modal.addEventListener('click', (e) => {
+      if (e.target.classList.contains('modal')) {
+        modal.classList.remove('clickedModal');
+      }
+    });
+  
+    modal.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+  }
+  
+  function fillModal(character) {
+    const contentModal = document.querySelector('.contentModal');
+    contentModal.innerHTML = `
+      <h2>${character.name}</h2>
+      <img class="modalImg" src="${character.img}" alt="${character.name}" />
+      <p>Clan: ${character.clan}</p>
+      <p>${character.about}</p>
+    `;
+  }
+  
+  function updateCards(arr) {
+    const $updatedCards = generateCardHTML(arr);
+    $carshinobiBottom.innerHTML = $updatedCards;
+    initEventListeners();
+  }
+  
+  const $inputClan = document.querySelector('.inputClan');
+  $inputClan.addEventListener('input', handleInputClan);
+  
+  const $inputName = document.querySelector('.inputName');
+  $inputName.addEventListener('input', handleInputName);
+  
+  let displayedShinobiWorld = [...shinobiWorld];
+  
+  function handleInputClan(e) {
+    let searchingItem = e.target.value.toLowerCase();
+    displayedShinobiWorld = shinobiWorld.filter((item) => item.clan.toLowerCase().includes(searchingItem));
+    updateCards(displayedShinobiWorld);
+  }
+  
+  function handleInputName(e) {
+    let searchingItem = e.target.value.toLowerCase();
+    displayedShinobiWorld = shinobiWorld.filter((item) => item.name.toLowerCase().includes(searchingItem));
+    updateCards(displayedShinobiWorld);
+  }
+  
+
+  updateCards(shinobiWorld);
+
+
+
+
+
+
+
+console.log(Object.keys(shinobiWorld).length)
